d034df97b68b0c98095a5f98e2581cbfd3e01511
Kazakoffnik <97301327+Kazakoffnik@users.noreply.github.com>
2022-03-16 20:57:31 +0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -3,18 +3,15 @@
 
 int main() {
   FILE *F1;
-  F1 = fopen("adjacency_matrix_90.txt", "r");//открываем нашу смежную матрицу
+  F1 = fopen("adjacency_matrix_90.txt", "r");
   int x[5][5];
   for (int i = 0; i <= 4; i++)
     for (int j = 0; j <= 4; j++)
       x[i][j] = 0;
-  for (int i = 0; i <= 4; i++) {
-    fscanf(F1, "%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3],
-           &x[i][4]);
-  }
+  for (int i = 0; i <= 4; i++)
+    fscanf(F1, "%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3],&x[i][4]);
   fclose(F1);
-  FILE *F2;
-  F2 = fopen("norm.dot", "a");
+  FILE *F2 = fopen("norm.dot", "a");;
   fprintf(F2, "graph G {\n");
   for (int i = 0; i <= 4; i++)
     for (int j = i; j <= 4; j++)
