0355d0cfa93bac0de9a41ea0d6b3ff940420aeec
Kazakoffnik <97301327+Kazakoffnik@users.noreply.github.com>
2022-03-16 17:34:49 +0300

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,25 @@
+//Я выбрал файл adjacency_matrix_90.txt
+#include <stdio.h>
+
+int main() {
+  FILE *F1;
+  F1 = fopen("adjacency_matrix_90.txt", "r");//открываем нашу смежную матрицу
+  int x[5][5];
+  for (int i = 0; i <= 4; i++)
+    for (int j = 0; j <= 4; j++)
+      x[i][j] = 0;
+  for (int i = 0; i <= 4; i++) {
+    fscanf(F1, "%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3],
+           &x[i][4]);
+  }
+  fclose(F1);
+  FILE *F2;
+  F2 = fopen("norm.dot", "a");
+  fprintf(F2, "digraph G {\n");
+  for (int i = 0; i <= 4; i++)
+    for (int j = i; j <= 4; j++)
+      for (int l = x[i][j]; l >= 1; l--)
+        fprintf(F2, "\tx%d -> x%d;\n", i + 1, j + 1);
+  fprintf(F2, "}");
+  fclose(F2);
+}
