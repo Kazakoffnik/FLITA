65bcda36a6595a0fefd45e6e8e64ae8855826e12
Kazakoffnik <97301327+Kazakoffnik@users.noreply.github.com>
2022-03-29 14:16:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,20 @@ int main()
   for (int i = 0; i <= 4; i++)
     fscanf(F1, "%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3], &x[i][4]);
   fclose(F1);
+  char c = '"';
+  FILE *F2 = fopen("norm.dot", "a+");
+  fprintf(F2, "graph G {\n");
+  for (int i = 0; i <= 4; i++)
+    if (x[i][0] == 0 && x[i][1] == 0 && x[i][2] == 0 && x[i][3] == 0 && x[i][4] == 0)
+    {
+      fprintf(F2, "\tx%d\n", i + 1);
+    }
+    else
+    {
+      for (int j = i; j <= 4; j++)
+        for (int l = x[i][j]; l >= 1; l--)
+          fprintf(F2, "\tx%d -- x%d;\n", i + 1, j + 1);
+    }
   int reb = 0;
   for (int i = 0; i <= 4; i++)
   {
@@ -26,9 +40,6 @@ int main()
   for (int i = 0; i <= 4; i++)
     for (int j = 0; j <= i - 1; j++)
       reb += x[i][j];
-  char c = '"';
-  FILE *F2 = fopen("norm.dot", "a+");
-  fprintf(F2, "graph G {\n");
   if (reb >= (((5 - 1) * (5 - 2)) / 2))
   {
     fprintf(F2, "\t0 [shape = plaintext,label = %cSvyazniy%c];\n", c, c);
@@ -37,17 +48,6 @@ int main()
   {
     fprintf(F2, "\t0 [shape = plaintext,label = %cNesvyazniy%c];\n", c, c);
   }
-  for (int i = 0; i <= 4; i++)
-    if (x[i][0] == 0 && x[i][1] == 0 && x[i][2] == 0 && x[i][3] == 0 && x[i][4] == 0)
-    {
-      fprintf(F2, "\tx%d\n", i + 1);
-    }
-    else
-    {
-      for (int j = i; j <= 4; j++)
-        for (int l = x[i][j]; l >= 1; l--)
-          fprintf(F2, "\tx%d -- x%d;\n", i + 1, j + 1);
-    }
   fprintf(F2, "}");
   fclose(F2);
   system("dot norm.dot -Tpng  -o norm.jpg");
