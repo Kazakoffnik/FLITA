3f3d2cf0f7351e5ebdb0da357ff5adee508251b0
Kazakoffnik <97301327+Kazakoffnik@users.noreply.github.com>
2022-04-06 19:11:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_SIZE 10
 
 int main()
 {
   FILE *F1;
   F1 = fopen("adjacency_matrix_90.txt", "r");
-  int x[5][5];
-  for (int i = 0; i <= 4; i++)
-    for (int j = 0; j <= 4; j++)
-      x[i][j] = 0;
-  for (int i = 0; i <= 4; i++)
-    fscanf(F1, "%d %d %d %d %d", &x[i][0], &x[i][1], &x[i][2], &x[i][3], &x[i][4]);
-  fclose(F1);
+  int x[MAX_SIZE][MAX_SIZE];
+  for (int i = 0; i <= MAX_SIZE - 1; i++)
+    for (int j = 0; j <= MAX_SIZE - 1; j++)
+      x[i][j] = -1;
+  char c1;
+  int n = 0, m = 0;
+  while (!feof(F1))
+  {
+    fscanf(F1, "%c", &c1);
+    if (c1 != ' ' && c1 != '\n' && c1 != '\0')
+    {
+      x[n][m] = c1 - '0';
+      m++;
+    }
+    else if (c1 == '\n')
+    {
+      n++;
+      m = 0;
+    }
+  }
+  n = m;
   char c = '"';
   FILE *F2 = fopen("norm.dot", "a+");
   fprintf(F2, "graph G {\n");
-  for (int i = 0; i <= 4; i++)
-    if (x[i][0] == 0 && x[i][1] == 0 && x[i][2] == 0 && x[i][3] == 0 && x[i][4] == 0)
-    {
-      fprintf(F2, "\tx%d\n", i + 1);
-    }
-    else
-    {
-      for (int j = i; j <= 4; j++)
-        for (int l = x[i][j]; l >= 1; l--)
-          fprintf(F2, "\tx%d -- x%d;\n", i + 1, j + 1);
-    }
-  int reb = 0;
-  for (int i = 0; i <= 4; i++)
+  for (int i = 0; i <= n - 1; i++)
   {
-    for (int j = 0; j <= 4; j++)
+    int p = 0;
+    for (int t = 0; t <= n - 1; t++)
+      if (x[i][t] == 0)
+        p++ ;
+      if (p == n)
+        {
+          fprintf(F2,"\tx%d;\n", i + 1);
+        }
+      else
+      {
+
+        for (int j = i; j <= m - 1; j++)
+          for (int l = x[i][j]; l >= 1; l--)
+            fprintf(F2, "\tx%d -- x%d;\n", i + 1, j + 1);
+      }
+  }
+  int reb = 0;
+  for (int i = 0; i <= n - 1; i++)
+  {
+    for (int j = 0; j <= m - 1; j++)
     {
       if (x[i][j] > 1)
         x[i][j] = 1;
@@ -37,10 +59,10 @@ int main()
         x[i][j] = 0;
     }
   }
-  for (int i = 0; i <= 4; i++)
+  for (int i = 0; i <= n - 1; i++)
     for (int j = 0; j <= i - 1; j++)
       reb += x[i][j];
-  if (reb >= (((5 - 1) * (5 - 2)) / 2))
+  if (reb >= (((n - 1) * (n - 2)) / 2))
   {
     fprintf(F2, "\t0 [shape = plaintext,label = %cSvyazniy%c];\n", c, c);
   }
